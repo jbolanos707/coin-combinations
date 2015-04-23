8b6e8dee23e6da73ed279555e2d5a04748038e91
ben cornelis & jennifer bolanos <student@epicodus.com>
2015-04-23 11:46:53 -0700

    Remove puts from until loop.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -7,16 +7,11 @@ class Fixnum
     i = 0
     until amount.==(0)
       coin_type = coin_types.at(i)
-puts "Coin type: #{coin_type}"
       coin_value = coin_values.fetch(coin_type)
-puts "Coin value: #{coin_value}"
       number_of_coins = amount./(coin_value)
-puts "Coins of this type: #{number_of_coins}"
       coin_counts[coin_type] = number_of_coins
       amount = amount - number_of_coins.*(coin_value)
-puts "Amount left: #{amount}"
       i = i + 1
-puts "Loop: #{i}"
     end
     coin_counts
   end
