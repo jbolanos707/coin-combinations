c6997e8f932fac6e855db6890cc4c6992e9b4334
ben cornelis & jennifer bolanos <student@epicodus.com>
2015-04-23 10:36:48 -0700

    coin_comb will work for multiples of 25, 10, 5

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,6 +1,18 @@
 class Fixnum
   define_method(:coin_comb) do
     number_of_quarters = self/25
-    coin_count = {'quarters' => number_of_quarters, 'dimes' => 0, 'nickels'=> 0, 'pennies' => 0}
+    number_of_dimes = self/10
+    number_of_nickels = self/5
+    # self.%(25) is the remainder when self is divided by 25
+    # self.%(25).eql?(0) is true when self is divible by 25, false otherwise
+    if self.%(25).eql?(0)
+      coin_count = {'quarters' => number_of_quarters, 'dimes' => 0, 'nickels'=> 0, 'pennies' => 0}
+    elsif self.%(10).eql?(0)
+      coin_count = {'quarters' => 0, 'dimes' => number_of_dimes, 'nickels'=> 0, 'pennies' => 0}
+    elsif self.%(5).eql?(0)
+      coin_count = {'quarters' => 0, 'dimes' => 0, 'nickels' => number_of_nickels, 'pennies' => 0}
+    elsif self.eql?(1)
+      coint_count = {'quarters' => 0, 'dimes'=> 0, 'nickels' => 0, 'pennies' => 1}
+    end
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,4 +5,20 @@ describe('Fixnum#coin_comb') do
   it('divides the number by 25 to get the number of quarters') do
     expect(75.coin_comb()).to(eq({'quarters'=>3, 'dimes'=>0, 'nickels' => 0, 'pennies' =>0}))
   end
+
+  it('divides the number by 10 to get the number of dimes') do
+    expect(20.coin_comb()).to(eq({'quarters' => 0, 'dimes' => 2, 'nickels' => 0, 'pennies' => 0}))
+  end
+
+  it('divides the number by 5 to get the number of nickels') do
+    expect(5.coin_comb()).to(eq({'quarters' => 0, 'dimes' => 0, 'nickels' => 1, 'pennies' => 0}))
+  end
+
+  it('returns 1 penny if called on 1') do
+    expect(1.coin_comb()).to(eq({'quarters' => 0, 'dimes' => 0, 'nickels' => 0, 'pennies' => 1}))
+  end
+
+  it('returns the optimal combination of coins') do
+    expect(117.coin_comb()).to(eq({'quarters' => 4, 'dime'=> 1, 'nickel' => 7, 'pennies' => 2}))
+  end
 end
